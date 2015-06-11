34048d2344f88a6bf0c989b6245b772b0b4d43b9
j- <jh@live.com.au>
2015-06-11 16:13:15 +1000

    Converted computed properties to get/set pattern

--- addon/array-limit.js
+++ addon/array-limit.js
@@ -5,37 +5,40 @@ var computed = Em.computed;
 var DEFAULT_LIMIT = Infinity;
 
 var ArrayLimit = Em.ArrayProxy.extend({
-	limit: computed(function (name, limit, old) {
-		// getter
-		if (arguments.length < 2) {
+	limit: computed({
+		get: function () {
 			return DEFAULT_LIMIT;
-		}
-		limit = Number(limit); // ensure limit is number
-		limit = Math.max(limit, 0); // do not allow negative limit
-		if (old === undefined) {
-			// being set for first time, no need to update
+		},
+		set: function (name, limit, old) {
+			limit = Number(limit); // ensure limit is number
+			limit = Math.max(limit, 0); // do not allow negative limit
+			if (old === undefined) {
+				// being set for first time, no need to update
+				return limit;
+			}
+			var diff = limit - old;
+			if (diff === 0) {
+				// no need to continue if no difference
+				return limit;
+			}
+			var content = this.get('content');
+			var arranged = this.get('arrangedContent');
+			var arrangedLength = get(arranged, 'length');
+			var addedCount = Math.max(diff, 0);
+			var removedCount = Math.max(-diff, 0);
+			var toAdd = content.slice(old, old + addedCount);
+			arranged.replace(arrangedLength - removedCount, removedCount, toAdd);
 			return limit;
 		}
-		var diff = limit - old;
-		if (diff === 0) {
-			// no need to continue if no difference
-			return limit;
-		}
-		var content = this.get('content');
-		var arranged = this.get('arrangedContent');
-		var arrangedLength = get(arranged, 'length');
-		var addedCount = Math.max(diff, 0);
-		var removedCount = Math.max(-diff, 0);
-		var toAdd = content.slice(old, old + addedCount);
-		arranged.replace(arrangedLength - removedCount, removedCount, toAdd);
-		return limit;
 	}),
 
-	arrangedContent: computed('content', function () {
-		var content = this.get('content');
-		var limit = this.get('limit');
-		var slice = content.slice(0, limit);
-		return Em.A(slice);
+	arrangedContent: computed('content', {
+		get: function () {
+			var content = this.get('content');
+			var limit = this.get('limit');
+			var slice = content.slice(0, limit);
+			return Em.A(slice);
+		}
 	}),
 
 	// process items removed
